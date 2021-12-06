f29d201e1047257672bbc308962cb96e0bf168e6
jpeg25 <jairo25509@hotmail.com>
2021-12-05 20:38:36 -0500

    useState array CR_D

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import Setup from "./tutorial/1-useState/setup/1-error-example";
+import Setup from "./tutorial/1-useState/setup/3-useState-array";
 
 function App() {
   return (
     <div className="container">
       <Setup />
-      <h1>yeahs</h1>
+      <h1>yeahso</h1>
     </div>
   );
 }
--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -1,8 +1,46 @@
-import React from 'react';
-import { data } from '../../../data';
+import React from "react";
+import { data } from "../../../data";
 
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = React.useState(data);
+
+  const deleteThis = (id) => {
+    const newPeople = people.filter((person) => {
+      return person.id !== id;
+    });
+
+    setPeople(newPeople);
+  };
+
+  return (
+    <>
+      {people.map((person) => {
+        const { id, name } = person;
+
+        return (
+          <div key={id} className="item">
+            <h4>{name}</h4>
+            <button
+              onClick={() => {
+                deleteThis(id);
+              }}
+            >
+              Erase me!
+            </button>
+          </div>
+        );
+      })}
+      <button
+        className="btn"
+        //Event handler definido dentro de JSX.
+        onClick={() => {
+          setPeople([]);
+        }}
+      >
+        delete all
+      </button>
+    </>
+  );
 };
 
 export default UseStateArray;
