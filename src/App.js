ab2e1311b38769e7f85473868cdea4493fa545e5
jpeg25 <jairo25509@hotmail.com>
2022-02-14 17:38:04 -0500

    proptypes and defaultProps.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import Setup from "./tutorial/11-react-router/setup";
+import Setup from "./tutorial/12-memo-useMemo-useCallback/setup";
 
 function App() {
   return (
     <div className="container">
       <Setup />
-      {/* <h1>yeahso</h1> */}
+      {/* <h1>yeahso</h1>  */}
     </div>
   );
 }
